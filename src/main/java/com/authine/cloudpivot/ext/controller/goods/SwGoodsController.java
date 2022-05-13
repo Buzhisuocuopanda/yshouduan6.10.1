12ae95509f4bee182c3042dc14a2a36d350555a5
liuhaoyaun <15698178812@163.com>
2022-05-13 14:06:32 +0800

    Merge remote-tracking branch 'origin/入库流程' into 入库流程

--- src/main/java/com/authine/cloudpivot/ext/controller/goods/SwGoodsController.java
+++ src/main/java/com/authine/cloudpivot/ext/controller/goods/SwGoodsController.java
@@ -147,10 +147,11 @@ public ResponseResult<List<SwGSlistVo>> getgslist(@RequestParam String id ){
         String stmp = "";
         for (int n = 0; n < b.length; n++) {
             stmp = (java.lang.Integer.toHexString(b[n] & 0XFF));
-            if (stmp.length() == 1)
+            if (stmp.length() == 1) {
                 hs = hs + "0" + stmp;
-            else
+            } else {
                 hs = hs + stmp;
+            }
         }
         return hs;
     }
